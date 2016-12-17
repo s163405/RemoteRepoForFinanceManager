bd96ca4b51b9c3e61bae6c55889824afaa0e196c
Ken <Ken@Ken_desktop>
2016-12-18 00:24:35 +0900

    収支のDateY/M/D(:int)のDBAccess変更処理完了
    次回からはListServlet,list.jspの編集に戻る

--- WEB-INF/src/DBSample/Expenses.java
+++ WEB-INF/src/DBSample/Expenses.java
@@ -6,52 +6,83 @@ public class Expenses {
 	private int rid; // RIDを保存
 	private UserData user;// userを保存
 	private INCCat category;// 収入カテゴリを保存
-	private Date date;// 支出日を保存
+	private int year;// 年
+	private int month;// 月
+	private int day;// 日
 	private int amount;// 金額を保存
-	private String place;//支出場所を保存
+	private String place;// 支出場所を保存
 	private String memo;// メモテキストを保存
+
 	public int getRid() {
 		return rid;
 	}
+
 	public void setRid(int rid) {
 		this.rid = rid;
 	}
+
 	public UserData getUser() {
 		return user;
 	}
+
 	public void setUser(UserData user) {
 		this.user = user;
 	}
+
 	public INCCat getCategory() {
 		return category;
 	}
+
 	public void setCategory(INCCat category) {
 		this.category = category;
 	}
-	public Date getDate() {
-		return date;
+
+	public int getYear() {
+		return year;
 	}
-	public void setDate(Date date) {
-		this.date = date;
+
+	public void setYear(int year) {
+		this.year = year;
 	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+
+	public int getDay() {
+		return day;
+	}
+
+	public void setDay(int day) {
+		this.day = day;
+	}
+
 	public int getAmount() {
 		return amount;
 	}
+
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
+
 	public String getPlace() {
 		return place;
 	}
+
 	public void setPlace(String place) {
 		this.place = place;
 	}
+
 	public String getMemo() {
 		return memo;
 	}
+
 	public void setMemo(String memo) {
 		this.memo = memo;
 	}
-
 
 }
