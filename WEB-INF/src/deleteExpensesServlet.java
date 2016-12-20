f2deefd5fb9f77cc1a5b69fbaa4c70867e468290
s163405 <s163405@ICS830.icsuser.nagaokaut.ac.jp>
2016-12-20 14:36:55 +0900

    3_Delete完了
    次は4_updateをやります

--- WEB-INF/src/deleteExpensesServlet.java
+++ WEB-INF/src/deleteExpensesServlet.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Calendar;
 import java.util.LinkedList;
 
 import javax.servlet.ServletException;
@@ -11,13 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import DBSample.Expenses;
-import DBSample.Income;
 import DataBase.ExpensesManager;
-import DataBase.IncomeManager;
 
-@WebServlet(name = "listServlet", urlPatterns = { "/listServlet" })
+@WebServlet(name = "deleteExpensesServlet", urlPatterns = { "/deleteExpensesServlet" })
 
-public class listServlet extends HttpServlet {
+public class deleteExpensesServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		doMain(req, res);
 	}
@@ -33,30 +30,13 @@ public class listServlet extends HttpServlet {
 		PrintWriter out = res.getWriter();
 		HttpSession session = req.getSession();
 
-		String yearMonth = "";
-		yearMonth = (String) req.getParameter("month");
-		if (yearMonth == null) {
-			Calendar calendar = Calendar.getInstance();// îNåéêÿÇËë÷Ç¶
-			yearMonth = calendar.get(Calendar.YEAR) + "-" + (calendar.get(Calendar.MONTH) + 1);
-		}
-
-		String[] YM = yearMonth.split("-", 0);// 2016-12Ç2016Ç∆12Ç…ï™ÇØÇÈ
-		int year = Integer.parseInt(YM[0]);
-		int month = Integer.parseInt(YM[1]);
-
 		ExpensesManager em = new ExpensesManager();
 
-		LinkedList EXPList = em.getEXPList(year, month);
+		Expenses exp= (Expenses) session.getAttribute("exp");
 
-		session.setAttribute("EXPList", EXPList);
+		em.delete(exp.getRid());
 
-		IncomeManager im = new IncomeManager();
-
-		LinkedList INCList = im.getINCList(year, month);
-
-		session.setAttribute("INCList", INCList);
-
-		req.getRequestDispatcher("/list.jsp").forward(req, res);
+		req.getRequestDispatcher("./listServlet").forward(req, res);
 
 	}
 }
--- confirmIncomeDelete.jsp
+++ confirmIncomeDelete.jsp
@@ -1,24 +1,25 @@
-<%@page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page contentType="text/html; charset=UTF-8"%>
 <%@ page session="true"%>
 <%@ page import="DBSample.*"%>
-<%@ page import="java.util.*"%>
 <%@ page import="java.text.NumberFormat"%>
-<HTML>
-<HEAD>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
 
 <%
 	Income inc = (Income) session.getAttribute("inc");
-
-	session.removeAttribute("type");
-
 	NumberFormat toYen = NumberFormat.getCurrencyInstance(); //通貨形式
 %>
-</HEAD>
-<BODY>
+
+</head>
+<body>
+
 	<div align="center">
-		<h1>収入の詳細情報</h1>
+		<h1 align="center">収入情報の削除</h1>
+
 		<hr>
-		<table border="1" bordercolor="black">
+<table border="1" bordercolor="black">
 			<tr>
 				<td bgcolor="#92d050" align="center" width=80>日付</td>
 				<td bgcolor="#fcd5b5" align="left" width=400><%=inc.getYear() + "/" + inc.getMonth() + "/" + inc.getDay()%>
@@ -41,23 +42,26 @@
 			</tr>
 		</table>
 		<br />
-		<table align="0">
+
+		<p>この情報を削除します。よろしいですか？</p>
+
+		<table>
 			<tr>
-				<td><form method=post action=update.jsp>
-						<input type=submit value=このアイテムの情報を修正する>
-					</form></td>
-				<td><form method=post action=confirmIncomeDelete.jsp>
-						<input type=submit value=このアイテムを削除する>
-					</form></td>
-			</tr>
-			<tr>
-				<td><form method=post action=listServlet>
-						<input type=submit value=一覧に戻る>
-					</form></td>
-				<td></td>
+				<td><FORM METHOD="POST" ACTION="deleteIncomeServlet">
+						<INPUT TYPE="SUBMIT" VALUE="はい" />
+					</FORM></td>
+				<td>
+					<FORM METHOD="POST" ACTION="IncomeDetails.jsp">
+						<INPUT TYPE="SUBMIT" VALUE="いいえ" />
+
+					</FORM>
+				</td>
 			</tr>
 		</table>
 
+
+
 	</div>
-</BODY>
-</HTML>
+
+</body>
+</html>
